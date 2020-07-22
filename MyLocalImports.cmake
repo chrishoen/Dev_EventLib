f1324bcf1f1d6b0ac2654f6dce93a37e90161d65
chrishoen <chris123hoen@gmail.com>
2020-07-22 15:17:25 -0400

    rename LogLib to EventLib

--- MyLocalImports.cmake
+++ MyLocalImports.cmake
@@ -2,15 +2,15 @@
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_lib_import_LogLib _target)
+function(my_lib_import_EventLib _target)
 
-   target_link_libraries(${_target} LogLib)
+   target_link_libraries(${_target} EventLib)
 
 endfunction()
 
-function(my_inc_import_LogLib _target)
+function(my_inc_import_EventLib _target)
 
-   target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:LogLib,INTERFACE_INCLUDE_DIRECTORIES>)
+   target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:EventLib,INTERFACE_INCLUDE_DIRECTORIES>)
 
 endfunction()
 
