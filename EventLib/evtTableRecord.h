025afbb4ead8f690ace06bfd8fe1ed0a198ad976
chrishoen <chris123hoen@gmail.com>
2020-07-23 21:03:33 -0400

    adding event table record

---
+++ EventLib/evtTableRecord.h
@@ -0,0 +1,102 @@
+#pragma once
+
+/*==============================================================================
+Provides a class definition of event table records.
+==============================================================================*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+#include "evtEventDefs.h"
+
+namespace Evt
+{
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+#include <time.h>
+#include "evtEventDefs.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This is a class that implments a record for enties in the event table. It
+// provides storage for event properties and strings. Some of the properties
+// are temporary, like time of arrival, and apply to occurrences of individual
+// events. Some of the properties are constant, like constant display string
+// definitions.
+
+class EventTableRecord
+{
+public:
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Permenant Members. These are assigned at initialization.
+
+   // Event identifier.
+   int mEvtId;
+
+   // Event type.
+   int mType;
+
+   // Show string for set.
+   char* mStringForSet;
+
+   // Show string for clear.
+   char* mStringForClear;
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Transient Members. These are set according to the occurrence of
+   // individual events.
+
+   // Time of arrival.
+   timespec mTOA;
+
+   // Sequence number. This is incremented for each individual event that
+   // occurs.
+   int mSeqNum;
+
+   // True if a type2 event was set. False if cleared. This is only used
+   // by type2 events. This is used to define active alarms.
+   bool mCState;
+
+   // Time difference for a type2 event between the toa of a set and
+   // the toa of a clear.
+   double mDuration;
+
+   // Two arguments that can be passed to and event. An example: The 
+   // temperature went above threshold, arg1 is the temperature.
+   char mArgString1[cMaxRecordArgSize];
+   char mArgString2[cMaxRecordArgSize];
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Methods.
+
+   // Constructor.
+   EventTableRecord();
+   void reset();
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Methods.
+
+   // Show.
+   void show();
+};
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+}//namespace
+
+
