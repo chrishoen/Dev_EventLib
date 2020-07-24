baca1fc414cee4bd0c524a0400c50579ceb2deb2
chrishoen <chris123hoen@gmail.com>
2020-07-24 17:00:11 -0400

    more

--- EventLib/evtEventTable_init.cpp
+++ EventLib/evtEventTable_init.cpp
@@ -42,6 +42,37 @@ void EventTable::initializeRecord(
 
 void EventTable::initialize()
 {
+   initializeRecord(
+      Evt::cEvtId_Test1,
+      Evt::cEvt_Type2,
+      Evt::cEvt_SevCritical,
+      "Set Event for Test1",
+      "Clear Event for Test1",
+      "Alarm for Test1");
+
+   initializeRecord(
+      Evt::cEvtId_Test2,
+      Evt::cEvt_Type2,
+      Evt::cEvt_SevCritical,
+      "Set Event for Test2",
+      "Clear Event for Test2",
+      "Alarm for Test2");
+
+   initializeRecord(
+      Evt::cEvtId_Test3,
+      Evt::cEvt_Type2,
+      Evt::cEvt_SevCritical,
+      "Set Event for Test3",
+      "Clear Event for Test3",
+      "Alarm for Test3");
+
+   initializeRecord(
+      Evt::cEvtId_Test4,
+      Evt::cEvt_Type2,
+      Evt::cEvt_SevCritical,
+      "Set Event for Test4",
+      "Clear Event for Test4",
+      "Alarm for Test4");
 }
 
 //******************************************************************************
