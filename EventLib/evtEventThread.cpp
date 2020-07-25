113142376203b4caf6735c50b405eaa27f8adc48
chrishoen <chris123hoen@gmail.com>
2020-07-25 10:02:48 -0400

    new event thread code for qcalls

--- EventLib/evtEventThread.cpp
+++ EventLib/evtEventThread.cpp
@@ -1,13 +1,14 @@
 /*==============================================================================
+File: someTest1Thread.cpp
+Description:
 ==============================================================================*/
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
+
 #include "stdafx.h"
 
-#include "cmnPriorities.h"
-#include "risAlphaDir.h"
 
 #define  _EVENTTHREAD_CPP_
 #include "evtEventThread.h"
@@ -22,221 +23,41 @@ namespace Evt
 
 EventThread::EventThread()
 {
-   BaseClass::setThreadName("EventLog");
-   BaseClass::setThreadPriority(Cmn::gPriorities.mEventThread);
+   // Set base class thread variables.
+   BaseClass::setThreadName("EventThread");
    BaseClass::setThreadPrintLevel(TS::PrintLevel(0, 3));
+   BaseClass::setThreadPriorityHigh();
 
-   // Initialize variables.
-   mFile = 0;
-   mWriteCount = 0;
-   mTerminateFlag = false;
-}
-
-EventThread::~EventThread()
-{
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-
-void EventThread::doFileOpenNew()
-{
-   // Open the log file.
-   mFile = fopen("c:/aaa_prime/EventLib/EventLog.txt", "w");
-
-   if (mFile == 0)
-   {
-      TS::print(0, "EventLogNew open FAIL");
-   }
-}
-
-void EventThread::doFileOpenAppend()
-{
-   // Open the log file.
-   mFile = fopen("c:/aaa_prime/EventLib/EventLog.txt", "a+");
-
-   if (mFile == 0)
-   {
-      TS::print(0, "EventLogAppend open FAIL");
-   }
-}
-
-void EventThread::doFileClose()
-{
-   if (mFile)
-   {
-      fclose(mFile);
-      mFile = 0;
-   }
-}
-
-void EventThread::doFileFlush()
-{
-   fflush(mFile);
-}
-
-void EventThread::doFileWriteTimeStamp()
-{
-   TString* tString = new TString("PROGRAM START/////////////////////////////////////////////////////////////");
-//   tString->sendToEventLog();
+   // Set qcalls.
+   mProcessEventRecordQCall.bind(this, &EventThread::executeProcessEventRecord);
 }
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
 // Thread init function. This is called by the base class immediately 
-// after the thread starts running. This opens the file.
+// after the thread starts running.
 
 void EventThread::threadInitFunction()
 {
-   Prn::print(Prn::View11, "EventThread*****************************************BEGIN");
-   // Initialize the string queue.
-   mStringQueue.initialize(cQueueSize);
-
-   // Open the log file.
-   doFileOpenAppend();
-
-   // Write a time stamp for the file open.
-   doFileWriteTimeStamp();
 }
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Thread exit function, base class overload. This closes the file.
+// Thread exit function. This is called by the base class immediately
+// before the thread is terminated.
 
-void  EventThread::threadExitFunction()
+void EventThread::threadExitFunction()
 {
-   // Finalize the string queue.
-   mStringQueue.finalize();
-
-   // Close the log file.
-   doFileClose();
-
-   Prn::print(Prn::View11, "EventThread*****************************************END");
 }
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Thread run function. This is called by the base class immediately 
-// after the thread init function. It runs a loop that waits on the
-// semaphore. When it wakes up, it reads a string from the string
-// queue and prints it.
 
-void EventThread::threadRunFunction()
+void EventThread::executeProcessEventRecord(EventRecord* aEventRecord)
 {
-   // Loop to wait for posted events and process them.
-   int tCount = 0;
-   while (true)
-   {
-      // Wait on the counting semaphore.
-      mSemaphore.get();
-
-      // Test for thread termination.
-      if (mTerminateFlag) break;
-
-      // Try to read a string from the queue.
-      if (TString* tString = (TString*)mStringQueue.tryRead())
-      {
-         // Print the string to the log file and then delete it.
-         printString(tString);
-      }
-   }
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// Thread shutdown function. Set the termination flag, post to the 
-// semaphore and wait for the thread to terminate.
-
-void EventThread::shutdownThread()
-{
-   shutdownThreadPrologue();
-
-   // Set the termination flag.
-   mTerminateFlag = true;
-   // Post to the semaphore.
-   mSemaphore.put();
-   // Wait for the thread run function to return.
-   BaseClass::waitForThreadTerminate();
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// Print a string to stdout and the program log file and delete it.
-// log file and then deletes it. This is called by the thread run function
-// when it dequeues a string from the queue.
-
-void EventThread::printString(TString* aString)
-{
-   // Local strings.
-   char tTemp[40];
-   char tLogString[cMaxStringSize];
-
-   // Copy the input timestamp to the log string.
-   strcpy(tLogString, aString->getTime(tTemp));
-
-   // Copy a prefix to the log string.
-
-   switch (aString->mLogCode)
-   {
-   case cLogAsAny:     strcat(tLogString, " $$$$$  "); break;
-   case cLogAsInput:   strcat(tLogString, " <<<    "); break;
-   case cLogAsNak:     strcat(tLogString, " nak    "); break;
-   case cLogAsOutput:  strcat(tLogString, " >>>>>  "); break;
-   case cLogAsSlice:   strcat(tLogString, " SLICE  "); break;
-   case cLogAsPWM:     strcat(tLogString, " PWM    "); break;
-   case cLogAsTest:    strcat(tLogString, " TEST   "); break;
-   case cLogAsCMarker: strcat(tLogString, " MARKER "); break;
-   case cLogAsInfo:    strcat(tLogString, " INFO   "); break;
-   case cLogAsError:   strcat(tLogString, " ERROR  "); break;
-   }
-
-   // Copy the input string to the log string.
-   strncat(tLogString, &aString->mString[0], cMaxStringSize);
-
-   // Copy a postfix to the log string.
-   strncat(tLogString, "\n", cMaxStringSize);
-
-   // Write the log string to the log file.
-   fputs(tLogString, mFile);
-
-   // Metrics.
-   mWriteCount++;
-
-   // Done.
-   delete aString;
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// Try to write a string to the to the queue. Return true if
-// successful. This is called by print invocations to enqueue a string.
-// It writes to the string queue and posts to the semaphore, which
-// then wakes up the thread run function to process the string queue.
-
-bool EventThread::tryWriteString(TString* aString)
-{
-   // Guard.
-   if (mTerminateFlag) return false;
-
-   // Try to write to the call queue.
-   if (!mStringQueue.tryWrite(aString))
-   {
-      // The write was not successful.
-      return false;
-   }
-
-   // Post to the semaphore.
-   mSemaphore.put();
-
-   // Successful.
-   return true;
 }
 
 //******************************************************************************
--- EventLib/evtEventThread.h
+++ EventLib/evtEventThread.h
@@ -1,96 +1,36 @@
 #pragma once
 
 /*==============================================================================
-Thread Services.
-These provide printing, logging, and error handling services that are
-based on having the controlling parameters located in thread local storage.
-
-print thread class.
+Event processing thread
 ==============================================================================*/
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
 
-#include <stdio.h>
 
-#include "risThreadsThreads.h"
-#include "risThreadsSynch.h"
-#include "risLCPointerQueue.h"
-
-#include "evtTString.h"
+#include "risThreadsQCallThread.h"
+#include "evtEventRecord.h"
 
 namespace Evt
 {
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This qcall thread provides serialized access to the event table and the
+// alarm list. It provides a qcall that sends an event record to the thread.
+// It processes the event record to update the event table and alarm list and
+// then update the event log file and the alarm list file.
 
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// This is a thread class that prints strings to stdout and to a program 
-// log file.
-//   
-// It contains a pointer queue and a counting semaphore. Threads that use
-// this thread as a service to print strings write a string pointer to
-// the queue and post to the semaphore. This thread waits on the semaphore
-// and, when it wakes up, reads a string from the queue and prints it.
-//
-// It inherits from BaseThread to obtain thread functionality.
-// 
-
-class EventThread : public Ris::Threads::BaseThread
+class  EventThread : public Ris::Threads::BaseQCallThread
 {
 public:
-   typedef Ris::Threads::BaseThread BaseClass;
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Constants.
-
-   static const int cMaxStringSize = 400;
-   static const int cQueueSize = 1000;
+   typedef Ris::Threads::BaseQCallThread BaseClass;
 
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************
    // Members.
-
-   // Pointer queue that contains pointers to strings. Print invocations
-   // enqueue strings to this queue. This thread dequeues strings from it and
-   // prints them to the stdout and the program log file.
-   Ris::LCPointerQueue mStringQueue;
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Members.
-
-   // Counting semaphore. The thread run function contains a loop that
-   // waits on this for a timer or an event. The timer provides for
-   // periodic exectution and the event can signify that the call queue is
-   // ready or it can signify for thread termination.
-   Ris::Threads::CountingSemaphore mSemaphore;
-
-   // If true the the thread will terminate at the next post to the
-   // semaphore.
-   bool mTerminateFlag;
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Members.
-
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Members.
-
-   // Log file descriptor.
-   FILE* mFile;
-
-   // Metrics.
-   int  mWriteCount;
 
    //***************************************************************************
    //***************************************************************************
@@ -99,76 +39,47 @@ public:
 
    // Constructor.
    EventThread();
-  ~EventThread();
 
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************
-   // Methods. Thread base class overloads.
+   // Methods.
 
    // Thread init function. This is called by the base class immediately 
    // after the thread starts running.
    void threadInitFunction() override;
 
-   // Thread run function. This is called by the base class immediately 
-   // after the thread init function. It runs a loop that waits on the
-   // semaphore. When it wakes up, it reads a string from the string
-   // queue and prints it.
-   void threadRunFunction() override;
-
    // Thread exit function. This is called by the base class immediately
    // before the thread is terminated.
    void threadExitFunction() override;
 
-   // Thread shutdown function. Set the termination flag, post to the 
-   // semaphore and wait for the thread to terminate.
-   void shutdownThread() override;
-
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************
    // Methods.
 
-   // Print a string to stdout and the program log file and delete it.
-   // log file and then deletes it. This is called by the thread run function
-   // when it dequeues a string from the queue.
-   void printString (TString* aString);
+   // Event record request qcall. Update the event table and the alarm list.
+   // Update the event log file and the alarm list file. Delete the event
+   // record when finiished.
+   Ris::Threads::QCall1<EventRecord*> mProcessEventRecordQCall;
 
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Methods.
-
-   // Try to write a string to the to the queue. Return true if
-   // successful. This is called by print invocations to enqueue a string.
-   // It writes to the string queue and posts to the semaphore, which
-   // then wakes up the thread run function to process the string queue.
-   bool tryWriteString(TString* aString);
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Methods.
-
-   // File open and close.
-   void doFileOpenNew();
-   void doFileOpenAppend();
-   void doFileWriteTimeStamp();
-   void doFileClose();
-   void doFileFlush();
+   // This is bound to the qcall.
+   void executeProcessEventRecord (EventRecord* aEventRecord);
 };
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Global singular instance.
+// Global instance
 
 #ifdef _EVENTTHREAD_CPP_
-         EventThread* gEventThread = 0;
+          EventThread* gEventThread;
 #else
-extern   EventThread* gEventThread;
+   extern EventThread* gEventThread;
 #endif
+
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
 }//namespace
+
