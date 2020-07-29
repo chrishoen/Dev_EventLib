4fefc5aad7335b88b507664cce8c94ab3adeca01
chrishoen <chris123hoen@gmail.com>
2020-07-29 10:40:26 -0400

    builds for beagle works

--- EventLib/cmnPriorities_linux.cpp
+++ EventLib/cmnPriorities_linux.cpp
@@ -22,21 +22,9 @@ namespace Cmn
 Priorities::Priorities()
 {
    mTsPrint           = Ris::Threads::Priority(-1, 20);
-   mEventTthread      = Ris::Threads::Priority(-1, 21);
-   mBinaryComm        = Ris::Threads::Priority( 3, 51);
-   mBinarySim         = Ris::Threads::Priority(-1, 35);
-   mSerialMsg         = Ris::Threads::Priority( 3, 80);
-   mGCodeComm         = Ris::Threads::Priority(-1, 60);
-   mSerialString      = Ris::Threads::Priority(-1, 80);
-   mGraphics          = Ris::Threads::Priority(-1, 40);
-   mScriptSeqLong     = Ris::Threads::Priority(-1, 50);
-   mScriptSeqShort    = Ris::Threads::Priority(-1, 70);
-
-   mControl           = Ris::Threads::Priority(-1, 40);
-   mControlUdp        = Ris::Threads::Priority(-1, 60);
-   mIsoch             = Ris::Threads::Priority(-1, 40);
-
-   mForceSerialRx     = Ris::Threads::Priority(-1, 80);
+   mEventThread       = Ris::Threads::Priority(-1, 60);
+   mRandomThread1     = Ris::Threads::Priority(-1, 60);
+   mRandomThread2     = Ris::Threads::Priority(-1, 60);
 }
 
 //******************************************************************************
